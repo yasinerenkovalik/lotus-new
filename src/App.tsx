85691d88f3b61d7fc62ad3a3713ab357a68850cb
Yasin Eren Kovalık <94734316+yasinerenkovalik@users.noreply.github.com>
2025-01-16 13:20:16 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,8 @@ function Layout({ children }: { children: React.ReactNode }) {
           <div className="logo">
             <h1>Lotus Organizasyon</h1>
           </div>
-          
+          <meta name="description" content="Lotus Organizasyon olarak, Kocaeli’de düğün, nişan ve özel organizasyonlar için profesyonel hizmet sunuyoruz. Hemen detayları inceleyin!">
+
           <button 
             className="mobile-menu-btn"
             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
